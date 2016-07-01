1e17f71ba44cc46736b49c731a40c3200da58028
Nick Fogle <nickfogle@gmail.com>
2016-07-01 01:28:46 -0400

    Add UUID.JS

---
+++ uuid.js
@@ -0,0 +1,60 @@
+// UUID Generator
+(function() {
+  // Private array of chars to use
+  var CHARS = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'.split('');
+
+  Math.xxid = function (len, radix) {
+    var chars = CHARS, xxid = [], i;
+    radix = radix || chars.length;
+
+    if (len) {
+      // Compact form
+      for (i = 0; i < len; i++) xxid[i] = chars[0 | Math.random()*radix];
+    } else {
+      // rfc4122, version 4 form
+      var r;
+
+      // rfc4122 requires these characters
+      xxid[8] = xxid[13] = xxid[18] = xxid[23] = '-';
+      xxid[14] = '4';
+
+      // Fill in random data.  At i==19 set the high bits of clock sequence as
+      // per rfc4122, sec. 4.1.5
+      for (i = 0; i < 36; i++) {
+        if (!xxid[i]) {
+          r = 0 | Math.random()*16;
+          xxid[i] = chars[(i == 19) ? (r & 0x3) | 0x8 : r];
+        }
+      }
+    }
+
+    return xxid.join('');
+  };
+
+  // A more performant, but slightly bulkier, RFC4122v4 solution. Limiting calls to random()
+  Math.xxidFast = function() {
+    var chars = CHARS, xxid = new Array(36), rnd=0, r;
+    for (var i = 0; i < 36; i++) {
+      if (i==8 || i==13 ||  i==18 || i==23) {
+        xxid[i] = '-';
+      } else if (i==14) {
+        xxid[i] = '4';
+      } else {
+        if (rnd <= 0x02) rnd = 0x2000000 + (Math.random()*0x1000000)|0;
+        r = rnd & 0xf;
+        rnd = rnd >> 4;
+        xxid[i] = chars[(i == 19) ? (r & 0x3) | 0x8 : r];
+      }
+    }
+    return xxid.join('');
+  };
+
+  // A more compact, but less performant, RFC4122v4 solution:
+  Math.xxidCompact = function() {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+      var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
+      return v.toString(16);
+    });
+  };
+})();
+
